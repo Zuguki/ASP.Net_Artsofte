b062c4641cef46cc111b44ab1a2af270ad24ad62
Zuguki <guskov.zahar20@gmail.com>
2022-09-07 19:44:27 +0300

    Clear main

--- Lecture/Lecture/Program.cs
+++ Lecture/Lecture/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace Lecture
+﻿namespace Lecture
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
