2caf66dca7b56f4b6c2d1cf8c7ac9f356b95791d
Zuguki <guskov.zahar20@gmail.com>
2022-09-18 17:30:46 +0300

    Refacttored

--- test/Program.cs
+++ test/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
